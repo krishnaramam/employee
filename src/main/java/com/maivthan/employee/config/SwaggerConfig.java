d452ebb4f693e0ceb367f833e69443df2b141e39
Ramakrishna Manchana <ramakrishna.m12@ad.infosys.com>
2020-12-08 19:17:39 +0530

    test

--- src/main/java/com/maivthan/employee/config/SwaggerConfig.java
+++ src/main/java/com/maivthan/employee/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.maivthan.employee.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,9 +16,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 	
-   
+	@Value("${SPRING_PROFILES_ACTIVE}")
+	private String region_env;
 	
-	
+
 	@Bean
 	public Docket API() {
 	    return new Docket(DocumentationType.SWAGGER_2)
@@ -30,8 +32,18 @@ public class SwaggerConfig {
 	private ApiInfo APIInfo() {
 		return new ApiInfoBuilder().title("Employee Micro Service" )				
 				.description("Maivthan Global Retail ")
-				.license("Project:Maivthan Retail")
+				.license("Region Env:"+region_env)
 				.version("1.0").build();
 	}		
+	
+	public String getRegion_env() {
+		return region_env;
+	}
+
+	public void setRegion_env(String region_env) {
+		this.region_env = region_env;
+	}
+
+	
 	
 }
