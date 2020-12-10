388d06dd55036fa59f19e78fc983e195bbe49215
Ramakrishna Manchana <ramakrishna.m12@ad.infosys.com>
2020-12-10 18:34:43 +0530

    test

--- bin/push.bat
+++
@@ -1,4 +0,0 @@
-git add -A
-git commit -m "test"
-git push --set-upstream https://manchanaramakrishna@dev.azure.com/manchanaramakrishna/maivthan/_git/employee-service main
-
--- bin/start.cmd
+++
@@ -1,1 +0,0 @@
-mvn clean spring-boot:run -Dspring-boot.run.jvmArguments="-Dserver.port=8090"
